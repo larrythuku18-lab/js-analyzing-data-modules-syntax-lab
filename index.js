e62fe29705b9c8923d430e7354b6925c49a9d96c
Larry Thuku <larrythuku18@gmail.com>
2026-04-15 11:46:12 +0300

    Worked on the The analyzing data with modules and Syntax

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
+function combineUsers(...args) {
+const combinedObject = args.reduce((arg, user) => {
+  return { ...arg, ...user };
+}, {});
+const merge_date = new Date().toLocaleDateString();
+return { ...combinedObject, merge_date };
+}
+  
 
 
 module.exports = {
